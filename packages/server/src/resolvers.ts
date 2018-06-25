d4d3faec31f6d2524c91ee943ecc97d854810356
Harshit Pant <pantharshit00@gmail.com>
2018-06-25 19:49:13 +0530

    feat: Add more types to schema (graphql)

--- packages/server/src/resolvers.ts
+++ packages/server/src/resolvers.ts
@@ -3,14 +3,6 @@ import { neo4jgraphql } from "neo4j-graphql-js";
 const typeDefs = {
   Query: {
     getUserById: neo4jgraphql
-  },
-  Mutation: {
-    DeleteUserById: () => {
-      return {
-        id: "sds",
-        name: "dsdaf"
-      };
-    }
   }
 };
 
--- packages/server/src/schema.ts
+++ packages/server/src/schema.ts
@@ -4,13 +4,25 @@ const typeDefs = gql`
   type User {
     id: ID!
     name: String!
+    email: String!
+    username: String!
+    githubToken: String!
+    createdAt: String
+    updatedAt: String
+    repositories: [Repository] @relation(name: "OWNER", direction: "OUT")
+  }
+
+  type Repository {
+    id: ID!
+    slug: String!
+    githubUrl: String!
+    githubName: String!
+    githubOwner: String!
+    owner: User @relation(name: "OWNER", direction: "IN")
   }
 
   type Query {
     getUserById(id: ID!): User
-  }
-  type Mutation {
-    DeleteUserById(id: ID): User
   }
 `;
 
