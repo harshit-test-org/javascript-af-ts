13ef05d37f8db033bbf7ce3c08d8ab69c6b818ea
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-07-13 19:15:25 +0200

    test(cypress): basic TS support
    no transpilation or cypress-testing-library support yet

--- cypress/e2e/basic.spec.js
+++
@@ -1,8 +0,0 @@
-describe("basic tests", () => {
-    it("can visit the homepage", () => {
-    cy.visit("/");
-  });
-  it('has a login DOM-node',()=>{
-      cy.visit('/').getByText('Login')
-  })
-});
